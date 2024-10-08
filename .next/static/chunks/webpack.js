5b1824098e62aade598075e89b5975bad46d6243
jiahaoliu666 <hao@haoMacBook-Pro.local>
2024-10-08 20:34:25 +0800

    v1.12

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f63073630043930e"; }
+/******/ 		__webpack_require__.h = function() { return "6355c69afaa04157"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,17 @@ import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";  
 import outputs from "@/amplify_outputs.json";  
 import "@aws-amplify/ui-react/styles.css";  
+import Head from 'next/head';  // 導入 Head  
 
 Amplify.configure(outputs);  
 
 export default function App({ Component, pageProps }: AppProps) {  
-  return <Component {...pageProps} />;  
+  return (  
+    <>  
+      <Head>  
+        <title>AppStream 登入系統</title>  {/* 設置標題 */}  
+      </Head>  
+      <Component {...pageProps} />  
+    </>  
+  );  
 }
